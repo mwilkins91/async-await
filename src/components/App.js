7ec1513a5d595a87df6a4e9f7b1b49288efee707
wilkinm <mark.wilkins@uberflip.com>
2018-04-14 19:23:27 -0400

    remove any async/awaits

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,9 @@ class App extends Component {
     pokemon: false,
     loading: false,
   };
+  
+  // Fetch a single user by ID from the server and store it in state
+  // Get the user's favorite pokemon from the pokemon API and store it in state
   getUser = (e, userId) => {
     e.preventDefault();
     this.setState({ loading: true });
@@ -34,22 +37,38 @@ class App extends Component {
         console.error(err);
       });
   };
-  async componentDidMount() {
-    const { data } = await axios.get('/users');
-    this.setState({
-      users: data,
-    });
+
+  // Get an array of all user's IDs/Names from the server to populate our form
+  componentDidMount() {
+    axios
+      .get('/users')
+      .then(res => {
+        this.setState({
+          users: res.data,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   render() {
     const { users, selectedUser, pokemon, loading } = this.state;
+
     if (!users.length || loading) {
+      // Show the loading screen if we haven't fetched any users yet
+      // Or if we're waiting for a request
       return <p className="loading is-size-1 title">Loading...</p>;
+
     } else if (!selectedUser || !pokemon) {
+      // Show the user select screen if we have no user or no pokemon
       return (
         <SelectUser handleSubmit={this.getUser} users={this.state.users} />
       );
+
     } else {
+      // Show the user info screen if we have everything!
       return <UserInfo user={selectedUser} pokemon={pokemon} />;
+
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import '../node_modules/bulma/css/bulma.min.css';
 import './styles.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
