8a8ef1b21d3d8985acdf774a4b77f59a6bcd352f
wilkinm <mark.wilkins@uberflip.com>
2018-04-14 19:45:00 -0400

    add spaceing

--- src/components/App.js
+++ src/components/App.js
@@ -55,17 +55,23 @@ class App extends Component {
     const { users, selectedUser, pokemon, loading } = this.state;
 
     if (!users.length || loading) {
+
       // Show the loading screen if we haven't fetched any users yet
       // Or if we're waiting for a request
       return <p className="loading is-size-1 title">Loading...</p>;
+
     } else if (!selectedUser || !pokemon) {
+
       // Show the user select screen if we have no user or no pokemon
       return (
         <SelectUser handleSubmit={this.getUser} users={this.state.users} />
       );
+
     } else {
+
       // Show the user info screen if we have everything!
       return <UserInfo user={selectedUser} pokemon={pokemon} />;
+      
     }
   }
 }
