331e3e393091b16c1c1fd78589537716c813dc5a
wilkinm <mark.wilkins@uberflip.com>
2018-04-22 12:50:48 -0400

    add aside

---
+++ aside.js
@@ -0,0 +1,13 @@
+
+
+// What is it? 
+// - Async & Await allow us to handle asyncronous code as though it were syncronous
+
+// * Currently in Stage 4
+
+// * Part of: ECMAScript 2017 (ES8)
+
+// * Supported in all major browsers minus IE/Edge (to some degree)
+
+// * Supported in Node 7.6 and highter
+
